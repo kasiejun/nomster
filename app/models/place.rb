022c378db96ea01123972bb2b15f1fa62111b9ad
Kasie Jun <jeoncas@gmail.com>
2016-01-06 02:02:08 +0000

    ready

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,9 @@ class Place < ActiveRecord::Base
 
 	belongs_to :user
 
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 4 }
+
+	validates :address, :presence => true
+
+	validates :description, :presence => true
 end
